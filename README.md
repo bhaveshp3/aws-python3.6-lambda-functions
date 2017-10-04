302e8ae0d3c20399da126976603f75f3818a64e1
bhaveshpatel <bhaveshp3@gmail.com>
2017-10-04 18:56:35 +0530

    Lambda Functions creation for AMI Management

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "#aws-python3.6-lambda-functions" 
+"# aws-python3.6-lambda-functions" 
--- labmda_function_create_ami_production_server.py
+++ labmda_function_create_ami_production_server.py
@@ -13,7 +13,7 @@ def lambda_handler(event, context):
     creationdate = datetime.datetime.now()
     creationdatetimestr =creationdate.strftime('%Y-%m-%d_%H-%M-%S')
 
-    instanceid = 'i-0c2f14c0ab0430147'  ## project Instance Id
+    instanceid = 'i-0xxxxxxxxxx7'  ## project Instance Id
 
     newimage = ec2client.create_image(Description="production-website_created_by_Lambda_function",InstanceId=instanceid,DryRun=False,Name="production-website-"+creationdatetimestr,NoReboot=True)
 
